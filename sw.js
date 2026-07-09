001ce5dc10d4697684f0f58f4d380fcab4e7d9c2
mercadinhoJL-eng <mercadinhocelina@gmail.com>
2026-07-08 21:05:09 -0300

    Create sw.js
    Tudo certo

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('jl-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
